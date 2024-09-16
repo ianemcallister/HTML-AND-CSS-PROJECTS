a8f03f735fb4dd141e5b96f641c0f0328cea018c
Ian McAllister <iemcallister@gmail.com>
2024-09-16 11:34:21 -0700

    Adding JQuery functionality to Bootstrap on Academy Cinemas

--- Academy-Cinemas/javascript/javascript.js
+++ Academy-Cinemas/javascript/javascript.js
@@ -45,3 +45,35 @@ var ticketQtTT = document.getElementById('quantity');
 var msTooltip = new bootstrap.Tooltip(movieSelectTT);
 var stTooltip = new bootstrap.Tooltip(showTimesTT);
 var tqTooltip = new bootstrap.Tooltip(ticketQtTT);
+
+
+// Shrinks header size when the document is scrolled down by 80 pixels
+$(document).on("scroll", function() {
+    // When the webpage is scrolled down from the top by 50px this 
+    // if statement will trigger
+    if ($(document). scrollTop() > 50) {
+        // Once the 50px requirement has been met and the
+        // nav-shrink class selector to the same HTML element
+        // that has the nav class
+        $('nav').addClass("nav-shrink");
+        // Adjust to position of the mobile drop menu
+        // to accomodate the new height diecrease
+        $("div.navbar-collapse").css('margin-top', '-6px');
+    } else {
+        // If the webpage has not been scrolled down or is back at the top of the nav-shrink
+        // class selector is removed from the HTML element with the nave class selector
+        $("nav").removeClass("nav-shrink");
+        // The margin for the drop down menu is now returned to it's original amount
+        $("div.navbar-collapse").css('margin-top', "14px");
+    }
+});
+
+// CLOSE MOBILE MENU WHEN A NAVIGATION LINK IS CLICKED
+$(document).ready(function() {
+    // On click when and element contains just the nav-link class and not the dropdown-toggle and then
+    // also close when an element with the class .dropdown-item (each movie link) has been clicked
+    $('.navbar-nav').on('click', '.nav-link:not(".dropdown-toggle"), .dropdown-item', function() {
+        // Collapse the navbar when a link or dropdown item is clicked
+        $(".navbar-collapse").collapse('hide');
+    });
+});
